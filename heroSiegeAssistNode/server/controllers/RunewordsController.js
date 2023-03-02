8afca3905c886707cacbed55f0b211ef5b83cb49
Abraham Coombs <coombsab@gmail.com>
2023-03-01 22:12:42 -0700

    basic runeword controller functions laid out

--- heroSiegeAssistNode/server/controllers/RunewordsController.js
+++ heroSiegeAssistNode/server/controllers/RunewordsController.js
@@ -5,11 +5,40 @@ export class RunewordsController extends BaseController {
     super("api/runewords")
     this.router
       .get("", this.getRunewords)
+      .post("", this.addRuneword)
+      .put("/:runewordId", this.editRuneword)
+      .delete("/:runewordId", this.deleteRuneword)
   }
 
   async getRunewords(req, res, next) {
     try {
       return res.send("Here are my runewords!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async addRuneword(req, res, next) {
+    try {
+      return res.send("Added a runeword!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+  async editRuneword(req, res, next) {
+    try {
+      return res.send("Edited the runeword!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async deleteRuneword(req, res, next) {
+    try {
+      return res.send("Deleted the runeword!")
     }
     catch(error) {
       next(error)
