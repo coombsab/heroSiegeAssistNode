c96ddb5824872049abff269fc33da274e2ca19c4
Abraham Coombs <coombsab@gmail.com>
2023-03-02 23:48:31 -0700

    layout of effects controller

---
+++ heroSiegeAssistNode/server/controllers/EffectsController.js
@@ -0,0 +1,48 @@
+import BaseController from "../utils/BaseController";
+
+export class EffectsController extends BaseController {
+  constructor() {
+    super("api/effects")
+    this.router
+      .get("", this.getEffectsText)
+      .post("", this.addEffectText)
+      .put("/:effectId", this.editEffectText)
+      .delete("/:effectId", this.deleteEffectText)
+  }
+
+  async getEffectsText(req, res, next) {
+    try {
+      res.send("Getting effects text!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async addEffectText(req, res, next) {
+    try {
+      res.send("Adding effect text!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async editEffectText(req, res, next) {
+    try {
+      res.send("Editing an effect text!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async deleteEffectText(req, res, next) {
+    try {
+      res.send("Deleting an effect text!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+}
