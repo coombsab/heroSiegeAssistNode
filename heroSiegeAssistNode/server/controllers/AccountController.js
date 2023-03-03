aa3f4cfd5b10643735523f77edc18224ce48d763
Abraham Coombs <coombsab@gmail.com>
2023-03-02 23:54:58 -0700

    layout of account controller

--- heroSiegeAssistNode/server/controllers/AccountController.js
+++ heroSiegeAssistNode/server/controllers/AccountController.js
@@ -8,6 +8,11 @@ export class AccountController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .get("/runes", this.getMyRunes)
+      .post("/runes", this.addToMyRunes)
+      .put("/runes/:runeId", this.editMyRune)
+      .delete("/runes/:runeId", this.deleteMyRune)
+      .delete("/runes", this.deleteMyRunes)
   }
 
   async getUserAccount(req, res, next) {
@@ -15,6 +20,51 @@ export class AccountController extends BaseController {
       const account = await accountService.getAccount(req.userInfo)
       res.send(account)
     } catch (error) {
+      next(error)
+    }
+  }
+
+  async getMyRunes(req, res, next) {
+    try {
+      res.send("Getting my runes!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async addToMyRunes(req, res, next) {
+    try {
+      res.send("Adding another rune to my list!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async editMyRune(req, res, next) {
+    try {
+      res.send("Editing one of my runes!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async deleteMyRune(req, res, next) {
+    try {
+      res.send("Deleting one of my runes!")
+    }
+    catch(error) {
+      next(error)
+    }
+  }
+
+  async deleteMyRunes(req, res, next) {
+    try {
+      res.send("Deleting all of my runes!!!")
+    }
+    catch(error) {
       next(error)
     }
   }
