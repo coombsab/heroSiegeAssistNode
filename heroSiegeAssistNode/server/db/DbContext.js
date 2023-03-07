b13c020810ac55f8b689ed7292512079f552f4bb
Abraham Coombs <coombsab@gmail.com>
2023-03-06 22:46:01 -0700

    start on dbcontext setup and runesservice

--- heroSiegeAssistNode/server/db/DbContext.js
+++ heroSiegeAssistNode/server/db/DbContext.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
+import { AbilitySchema } from "../models/Ability";
 import { AccountSchema } from '../models/Account'
+import { AccountsSchema } from "../models/Accounts";
+import { EffectTextSchema } from "../models/EffectText";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Abilities = mongoose.model("Ability", AbilitySchema);
+  Accounts = mongoose.model("Accounts", AccountsSchema);
+  EffectsText = mongoose.model("EffectText", EffectTextSchema)
 }
 
 export const dbContext = new DbContext()
---
+++ heroSiegeAssistNode/server/services/RunesService.js
@@ -0,0 +1,8 @@
+class RunesService {
+  async getRunes() {
+    
+  }
+
+}
+
+export const runesService = new RunesService()
