aeda1d97929a0b833ed7cdae33ef82fc96a141c4
Abraham Coombs <coombsab@gmail.com>
2023-03-07 07:14:33 -0700

    database models added to dbcontext

--- heroSiegeAssistNode/server/db/DbContext.js
+++ heroSiegeAssistNode/server/db/DbContext.js
@@ -3,6 +3,14 @@ import { AbilitySchema } from "../models/Ability";
 import { AccountSchema } from '../models/Account'
 import { AccountsSchema } from "../models/Accounts";
 import { EffectTextSchema } from "../models/EffectText";
+import { ItemSchema } from "../models/Item";
+import { MyRuneSchema } from "../models/MyRune";
+import { RuneSchema } from "../models/Rune";
+import { RuneRunewordSchema } from "../models/RuneRuneword";
+import { RunewordSchema } from "../models/Runeword";
+import { RunewordAbilitySchema } from "../models/RunewordAbility";
+import { RunewordEffectSchema } from "../models/RunewordEffect";
+import { RunewordItemSchema } from "../models/RunewordItem";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
@@ -11,6 +19,14 @@ class DbContext {
   Abilities = mongoose.model("Ability", AbilitySchema);
   Accounts = mongoose.model("Accounts", AccountsSchema);
   EffectsText = mongoose.model("EffectText", EffectTextSchema)
+  Items = mongoose.model("Items", ItemSchema)
+  MyRunes = mongoose.model("MyRune", MyRuneSchema)
+  Runes = mongoose.model("Rune", RuneSchema)
+  RuneRunewords = mongoose.model("RuneRuneword", RuneRunewordSchema)
+  Runewords = mongoose.model("Runeword", RunewordSchema)
+  RunewordAbilities = mongoose.model("RunewordAbility", RunewordAbilitySchema)
+  RunewordEffects = mongoose.model("RunewordEffect", RunewordEffectSchema)
+  RunewordItems = mongoose.model("RunewordItem", RunewordItemSchema)
 }
 
 export const dbContext = new DbContext()
