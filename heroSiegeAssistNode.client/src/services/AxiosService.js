76505f63563acc918369eb9309d660b9942f8459
Abraham Coombs <coombsab@gmail.com>
2023-04-11 11:20:33 -0600

    longer timeout on axiosservice

--- heroSiegeAssistNode.client/src/services/AxiosService.js
+++ heroSiegeAssistNode.client/src/services/AxiosService.js
@@ -2,5 +2,6 @@ import Axios from 'axios'
 import { baseURL } from '../env'
 export const api = Axios.create({
   baseURL,
+  // TODO Research optimization for mongodb calls to see if size can be decreased
   timeout: 16000
 })
